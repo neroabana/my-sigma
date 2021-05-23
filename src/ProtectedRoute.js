b4401683087617666c5275a34ce47f0c7fe0085f
abana <neroabana@gmail.com>
2021-05-23 06:49:04 +0100

    something in protectedRoutes

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 
 const ProtectedRoute = ({path, component, authenticated}) => {
-  return authenticated ? <Route path={path} component = {component} /> : <Redirect to = {`/`} /> 
+ 
+ return authenticated ? <Route path={path} component = {component} /> : <Redirect to = {`/`} /> 
+
 };
 
 export default ProtectedRoute;
