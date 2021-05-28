ab7ca510e79ee0937d7b592ba2b414067e9d40f6
abana <neroabana@gmail.com>
2021-05-28 14:39:51 +0100

    optimized protected route

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 
-const ProtectedRoute = ({path, component, authenticated}) => {
- 
+const ProtectedRoute = ({path, component, authenticated}) => { 
  return authenticated ? <Route path={path} component = {component} /> : <Redirect to = {`/`} /> 
-
 };
 
 export default ProtectedRoute;
