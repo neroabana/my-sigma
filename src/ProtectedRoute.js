25691e38cb32b6613003d1ca228fc4baad52712a
abana <neroabana@gmail.com>
2021-09-01 17:33:17 +0100

    edited protected route

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -4,5 +4,4 @@ import {Route, Redirect} from 'react-router-dom';
 const ProtectedRoute = ({path, component, authenticated}) => { 
  return authenticated ? <Route path={path} component = {component} /> : <Redirect to = {`/`} /> 
 };
-
 export default ProtectedRoute;
